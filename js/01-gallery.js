0308378cff50bc7738522ad58fb6912ca2cfcc55
Mihail <Maria@MihailMichael>
2023-06-17 19:06:34 +0300

    goit-js-hw-07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,36 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const items = [];
+galleryItems.forEach(element =>{
+    const photo = document.createElement('div')
+    photo.className = 'photo__item'
+    const photoLink = document.createElement('a')
+    photoLink.className = 'photo__link'
+    photoLink.href = element.original
+    const photoItem = document.createElement('img')
+    photoItem.className = 'gallery__image'
+    photoItem.src = element.preview;
+    photoItem.setAttribute('data-source', element.original)
+    photoItem.alt = element.description;
+    photo.append(photoLink);
+    photoLink.append(photoItem)
+    items.push(photo)
+})
+gallery.append(...items)
+gallery.addEventListener('click', event =>{
+    event.preventDefault();
+    if(event.target.nodeName !== 'IMG'){
+        return
+    }
+    const selectPhoto = event.target.getAttribute('data-source')
+    const instance = basicLightbox.create(`<img src = "${selectPhoto}" width="800" height= "600">`)
+    instance.show()
+    gallery.addEventListener('keydown', event => {
+        if(event.key === 'Escape'){
+            instance.close()
+        }
+    })
+})
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const items = [];
+galleryItems.forEach(element =>{
+    const photoLink = document.createElement('a')
+    photoLink.className = 'photo__link'
+    photoLink.href = element.original
+    const photoItem = document.createElement('img')
+    photoItem.className = 'gallery__image'
+    photoItem.src = element.preview;
+    photoItem.setAttribute('title', element.description)
+    photoItem.alt = element.description;
+    photoLink.append(photoItem)
+    items.push(photoLink)
+})
+gallery.append(...items)
+new SimpleLightbox('.gallery a',{
+    captionDelay: 250
+})
